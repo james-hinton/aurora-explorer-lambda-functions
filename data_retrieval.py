581b4d0a111a1bc71e987cad4b7de99eab4b99ca
James Hinton <james.hinton@live.co.uk>
2024-03-02 20:57:27 +0000

    feat: add data processing functions and data retrieval functions

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,9 +1,21 @@
+import requests
+import json
+
+
 def fetch_aurora_data():
     # Fetch data from Aurora
-    return {}
+    response = requests.get(
+        "https://services.swpc.noaa.gov/json/ovation_aurora_latest.json"
+    )
+    data = response.json()
+    return data
+
 
 def fetch_hpi_data():
     # Fetch data from HPI
-    return {}
+    response = requests.get(
+        "https://services.swpc.noaa.gov/text/aurora-nowcast-hemi-power.txt"
+    )
+    data = response.text
 
-
+    return data
