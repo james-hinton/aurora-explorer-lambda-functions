b5255ee6115f301e5527be88b322e5e76fb3b74d
James Hinton <james.hinton@live.co.uk>
2024-03-04 17:27:01 +0000

    refactor: S3 upload function to support custom upload directory

--- handler.py
+++ handler.py
@@ -1,19 +1,23 @@
 from data_retrieval import fetch_aurora_data, fetch_hpi_data
 from storage import store_in_s3
 
+
 def lambda_handler(event, context):
     aurora_data = fetch_aurora_data()
     hpi_data = fetch_hpi_data()
 
-    store_in_s3(aurora_data, "aurora-explorer-data", "aurora_data_gridded.json")
-    store_in_s3(hpi_data, "aurora-explorer-data", "aurora-hemi-power.txt")
+    store_in_s3(
+        aurora_data,
+        "aurora-explorer-data",
+        "aurora-data-raw",
+        "aurora-data-gridded.json",
+    )
+    store_in_s3(
+        hpi_data, "aurora-explorer-data", "aurora-hemi-raw", "aurora-hemi-power.txt"
+    )
 
-    return {
-        "statusCode": 200,
-        "body": "Data retrieved and stored in S3."
-    }
+    return {"statusCode": 200, "body": "Data retrieved and stored in S3."}
 
 
 if __name__ == "__main__":
     lambda_handler(None, None)
-
--- storage.py
+++ storage.py
@@ -3,19 +3,28 @@ import io
 from datetime import datetime
 import json
 
-def store_in_s3(data, bucket_name, object_name):
+
+def store_in_s3(data, bucket_name, upload_dir, object_name):
     """
     Uploads any data as a file to an S3 bucket.
-    
+
     Args:
     - data: The data to be uploaded (can be a dict for JSON or plain text).
     - bucket_name: The name of the S3 bucket.
     - object_name: The name of the object to be stored in the S3 bucket.
     """
-    s3_client = boto3.client('s3')
-    unique_name_with_timestamp = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + object_name
-    
-    print('Uploading data to S3:', unique_name_with_timestamp)
+    s3_client = boto3.client("s3")
+    object_name_prefix = object_name.split(".")[0]
+    object_name_suffix = object_name.split(".")[1]
+    unique_name_with_timestamp = (
+        object_name_prefix
+        + "_"
+        + datetime.now().strftime("%Y%m%d%H%M%S")
+        + "."
+        + object_name_suffix
+    )
+    full_target_path = upload_dir + "/" + unique_name_with_timestamp
+    print("Uploading data to S3:", unique_name_with_timestamp)
 
     # For JSON data, convert to string and encode to bytes
     if isinstance(data, dict):
@@ -25,6 +34,6 @@ def store_in_s3(data, bucket_name, object_name):
         file_like_object = io.BytesIO(data.encode())
 
     # Upload the bytes to S3
-    s3_client.upload_fileobj(file_like_object, bucket_name, unique_name_with_timestamp)
-    
-    print('Data uploaded successfully.')
+    s3_client.upload_fileobj(file_like_object, bucket_name, full_target_path)
+
+    print("Data uploaded successfully.")
